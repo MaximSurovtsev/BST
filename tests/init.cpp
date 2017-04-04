f2fc94c1f60f947ae304edeb8b52525b2991c261
MaximSurovtsev <gr4m0zeka@gmail.com>
2017-04-04 08:04:55 +0300

    Create init.cpp

---
+++ tests/init.cpp
@@ -0,0 +1,47 @@
+#include <catch.hpp>
+
+SCENARIO ("init", "[init]")
+{
+  BST<int> test;
+  REQUIRE(test.root_() == NULL);
+  REQUIRE(test.get_count() == 0);
+}
+
+SCENARIO("insert", "[init]")
+{
+  BST<int> test;
+  test.insert(4);
+  REQUIRE(test.get_count() == 1);
+  REQUIRE(test.search_result(4) == 1);
+}
+
+SCENARIO("get_pointer", "[init]")
+{
+  BST<int> test;
+  test.insert(4);
+  REQUIRE(test.get_pointer(4, test.root_()) != 0);
+}
+
+SCENARIO("get root", "[init]")
+{
+  BST<int> test;
+  test.insert(4);
+  REQUIRE(test.root_() != 0);
+}
+SCENARIO ("reading", "[init]")
+{
+   BST<int> test;
+   test.reading("file.txt");
+   REQUIRE(test.search_result(1) == 1);
+   REQUIRE(test.get_count() == 1);
+}
+
+SCENARIO ("writing", "[init]")
+{
+  BST<int> test;
+  test.insert(4);
+  test.writing("file1.txt");
+  BST<int> test2;
+  test.reading("file1.txt");
+  REQUIRE(test.get_count() == test2.get_count());
+}
